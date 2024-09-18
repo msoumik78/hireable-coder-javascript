2f9832bd3e8b6d3b87d263c14afa3b77c19c4ca3
Soumik Mukherjee <msoumik78@gmail.com>
2024-09-18 21:38:02 +0530

    updated subject

--- nats-client.js
+++ nats-client.js
@@ -1,27 +1,11 @@
-//import { connect, StringCodec } from "nats";
 const { connect, StringCodec } = require("nats");
-
-
-// to create a connection to a nats-server:
 
 (async () => {
     const nc = await connect({ servers: "localhost:4222" });
-
-    // create a codec
     const sc = StringCodec();
-    // create a simple subscriber and iterate over messages
-    // matching the subscription
-    const sub = nc.subscribe("subject");    
+    const sub = nc.subscribe("test-subject");    
   for await (const m of sub) {
     console.log(`[${sub.getProcessed()}]: ${sc.decode(m.data)}`);
   }
   console.log("subscription closed");
 })();
-
-// we want to ensure that messages that are in flight
-// get processed, so we are going to drain the
-// connection. Drain is the same as close, but makes
-// sure that all messages in flight get seen
-// by the iterator. After calling drain on the connection
-// the connection closes.
-//await nc.drain();
